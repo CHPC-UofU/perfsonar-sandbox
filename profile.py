d1b983506a4aad4c5d69bd1ee98b3394f37177d1
Adam H Griffith <adam.h.griffith@utah.edu>
2023-06-07 10:41:39 -0600

    Next iteration of CloudLab profile.py.

--- profile.py
+++ profile.py
@@ -9,41 +9,44 @@ Click on any node in the topology and choose the `shell` menu item. When your sh
 import geni.portal as portal
 # Import the ProtoGENI library.
 import geni.rspec.pg as pg
-# Import the Emulab specific extensions.
+# Emulab specific extensions.
 import geni.rspec.emulab as emulab
 
-# Define OS image
-OS_IMAGE = 'urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD'
-
-# Create a portal context.
+# Create a portal context, needed to define parameters
 pc = portal.Context()
 
 # Create a Request object to start building the RSpec.
 request = pc.makeRequestRSpec()
 
-# Add a raw Archive PC to the request and give it an interface.
-archivePC = request.RawPC("archive")
-archivePC.disk_image = OS_IMAGE
-ifaceArchivePC = archivePC.addInterface()
-ifaceArchivePC.addAddress(pg.IPv4Address("192.168.1.1", "255.255.255.0"))
+# Pick your OS.
+imageList = [
+    ('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD', 'CENTOS7-64-STD')]
 
-# Add a raw Testpoint PC to the request and give it an interface.
-testpointPC = request.RawPC("testpoint")
-testpointPC.disk_image = OS_IMAGE
-ifaceTestpointPC = testpointPC.addInterface()
-ifaceTestpointPC.addAddress(pg.IPv4Address("192.168.1.2", "255.255.255.0"))
+# Create, bind, and verify some user-configurable parameters.
+pc.defineParameter('osImage', "Select OS image",
+                   portal.ParameterType.IMAGE,
+                   imageList[0], imageList)
+params = pc.bindParameters()
+pc.verifyParameters()
 
-# Add a raw Toolkit PC to the request and give it an interface.
-toolkitPC = request.RawPC("toolkit")
-toolkitPC.disk_image = OS_IMAGE
-ifaceToolkitPC = toolkitPC.addInterface()
-ifaceToolkitPC.addAddress(pg.IPv4Address("192.168.1.3", "255.255.255.0"))
+# Add an Archive VM to the request that can be accessed from the public Internet.
+archive = request.XenVM("archive")
+archive.disk_image = params.osImage
+archive.routable_control_ip = True
 
-# Add L1 link between all nodes.
-link1 = request.L1Link("link1")
-link1.addInterface(ifaceArchivePC)
-link1.addInterface(ifaceTestpointPC)
-link1.addInterface(ifaceToolkitPC)
+# Add a Testpoint VM to the request that can be accessed from the public Internet.
+testpoint1 = request.XenVM("testpoint1")
+testpoint1.disk_image = params.osImage
+testpoint1.routable_control_ip = True
+
+# Add a Toolkit VM to the request that can be accessed from the public Internet.
+toolkit = request.XenVM("toolkit")
+toolkit.disk_image = params.osImage
+toolkit.routable_control_ip = True
+
+# Create a best effort LAN between them.
+lan = request.LAN(members=[archive, testpoint1, toolkit])
+lan.best_effort = True
 
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
